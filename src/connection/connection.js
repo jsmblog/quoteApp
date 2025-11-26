8e4f3c036f4a53ca2c85e800ec4dbec459b44b91
jsmblog <jsmblog19@gmail.com>
2025-11-26 10:28:43 -0500

    fix: update backend URL

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
+
 const API_BASE_URL = 'http://18.226.165.214/api';
+
 export const connection_to_backend = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true
 });
