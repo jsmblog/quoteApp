32a004615044d2098c7a53384c076a1a7a5ae24d
jsmblog <jsmblog19@gmail.com>
2025-11-26 09:44:43 -0500

    tird commit

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://18.226.165.214/api';
+const API_BASE_URL = '/api';
 export const connection_to_backend = axios.create({
   baseURL: API_BASE_URL,
 });
