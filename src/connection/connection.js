f93841f4c096c75de4087b28bdd78105a4fd3858
jsmblog <jsmblog19@gmail.com>
2025-11-26 10:27:50 -0500

    delete netlify.toml and fixed connection

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'http://18.226.165.214/api';
 export const connection_to_backend = axios.create({
   baseURL: API_BASE_URL,
 });
